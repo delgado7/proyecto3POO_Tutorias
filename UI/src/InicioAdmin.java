46969f8e5fb81bf8dad9e5d4a46fb49571da3b1b
sirodriguez11 <66538354+sirodriguez11@users.noreply.github.com>
2020-12-09 19:44:20 -0600

    Inicio UI

---
+++ UI/src/InicioAdmin.java
@@ -0,0 +1,28 @@
+import java.awt.Container;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JTextField;
+
+public final class InicioAdmin extends JFrame implements ActionListener{
+
+    JButton botonRevisarEstadisticas = new JButton("Revisar estadísticas");
+    JButton botonHabilitarAula = new JButton("Habilitar Aula");
+    JButton botonRegistrarTutor = new JButton("Registrar Tutor");
+    JButton botonSalir = new JButton("Salir");
+
+    InicioAdmin()
+    {
+
+    }
+    public void actionPerformed(ActionEvent e)
+    {
+
+    }
+}
+
